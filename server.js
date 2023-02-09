1c14e86c90fd66544ac8bc75fa621eecdf7c0d84
HieuNguyen1905 <120067573+HieuNguyen1905@users.noreply.github.com>
2023-02-09 11:24:38 -0800

    create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+//const session = require('express-session');
+//const exphbs = require('express-handlebars');
+const allRoutes = require('./controllers');
+
+const sequelize = require('./config/connection');
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(allRoutes);
+
+sequelize.sync({force:false}).then(function() {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  
