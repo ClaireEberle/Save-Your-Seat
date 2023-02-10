f044242b1512668f676ececdc248916266dcede3
ber <ber@berdeMacBook-Pro.local>
2023-02-09 16:11:02 -0800

    date picker is done

--- public/js/userview2-1.js
+++ public/js/userview2-1.js
@@ -1,10 +1,10 @@
+//* datepicker
+$( function() {
+    $( "#datepicker" ).datepicker();
+  })
 
 //* drop down
 $( function() {
     $( "#speed" ).selectmenu();
 })
 
-//* datepicker
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
