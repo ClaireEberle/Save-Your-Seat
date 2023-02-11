0f248283e6e525a80a0124c47002849f34bbea13
Sidney Basa <basa206@hotmail.com>
2023-02-10 20:41:07 -0800

    Fixed reservation-seeds and reservation model

--- seeds/reservation-seeds.js
+++ seeds/reservation-seeds.js
@@ -2,9 +2,11 @@ const {Reservation} = require('../models');
 
 const reservationData = [
     {
-        time_slots: "dinner",
-        day: "friday",
-        customer_id: 1
+        reservation_date: "2025-02-10",
+        reservation_time: "15:00",
+        party_size: 2,
+        customer_id: 1,
+        // owner_id: 1
     },
 ];
 
